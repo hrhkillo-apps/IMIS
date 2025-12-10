c1963448417db54e74d17aa36431eeb4c463394b
hrhkillo <hrhkillo@gmail.com>
2025-12-10 23:11:42 +0530

    feat: Add application constants for column requirements, data clearing, predefined links, and ID ranges.

--- id-generator-app/src/constants.js
+++ id-generator-app/src/constants.js
@@ -14,7 +14,9 @@ export const PREDEFINED_LINKS = [
     { label: 'CFMS Bill Status', url: 'https://prdcfms.apcfss.in:44300/sap/bc/ui5_ui5/sap/zexp_billstatus/index.html?sap-client=%27%27' },
     { label: 'Create Multiple Beneficiary Requests', url: 'https://prdcfms.apcfss.in:44300/sap/bc/ui5_ui5/sap/zexp_bf_mul_req/index.html' },
     { label: 'SBM IMIS', url: 'https://sbm.gov.in/SBMPhase2/Secure/Entry/UserMenu.aspx' },
-    { label: 'SAC', url: 'https://sac.ap.gov.in/internal' }
+    { label: 'SAC', url: 'https://sac.ap.gov.in/internal' },
+    { label: 'Tally xlsx to xlsx', url: '#' },
+    { label: 'Tally cfms xlsx to local xlsx', url: '#' }
 ];
 
 export const ID_RANGES = {
